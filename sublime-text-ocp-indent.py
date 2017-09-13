1f1af316f4dc55f018a8f58e89a0aa2e17c8a3d3
Jordan Scales <scalesjordan@gmail.com>
2017-09-13 14:23:53 -0400

    re-indent the entire code on save

--- sublime-text-ocp-indent.py
+++ sublime-text-ocp-indent.py
@@ -10,11 +10,43 @@ import re
 import os
 import sys
 import subprocess
+import platform
 
-supported_syntaxes = ['Packages/OCaml/OCaml.sublime-syntax']
+supported_syntaxes = ['Packages/OCaml/OCaml.sublime-syntax', 'Packages/OCaml/OCaml.tmLanguage']
+
+IS_WINDOWS = platform.system() == 'Windows'
+
+def do_replace(edit, view, region, replacement):
+    encoding = view.encoding() if view.encoding() != 'Undefined' else 'utf-8'
+    view.replace(edit, region, replacement.decode(encoding))
+
+def ocp_indent_code(edit, view, region):
+    command = ['ocp-indent']
+    code = view.substr(region)
+    proc = subprocess.Popen(
+        command,
+        stdin=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        shell=IS_WINDOWS
+    )
+    ocp_indent_output, err = proc.communicate(str.encode(code))
+
+    if err or proc.returncode != 0:
+        report_error(proc.returncode, err)
+    else:
+        do_replace(edit, view, region, ocp_indent_output)
+
+class OcpIndentCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        settings = self.view.settings()
+        if not settings.get('syntax') in supported_syntaxes:
+            return None
+
+        region = sublime.Region(0, self.view.size())
+        ocp_indent_code(edit, self.view, region)
 
 class OcpIndentBuffer(sublime_plugin.EventListener):
-
     def on_text_command(self, view, command_name, args):
         settings = view.settings()
         if not settings.get('syntax') in supported_syntaxes:
@@ -38,3 +70,6 @@ class OcpIndentBuffer(sublime_plugin.EventListener):
                 indented = '\n' + (result * ' ')
                 return (command_name, {'characters': indented})
         return None
+
+    def on_pre_save(self, view):
+        view.run_command("ocp_indent")
